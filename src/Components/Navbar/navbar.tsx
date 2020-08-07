508a8d978b101530722aa71872734d670b257850
SilentDubich <kirill.dubov.2012@mail.ru>
2020-08-07 21:29:46 +0300

    added free account and updated readme

--- src/Components/Navbar/navbar.tsx
+++ src/Components/Navbar/navbar.tsx
@@ -8,13 +8,13 @@ export const Navbar: FC<any> = props => {
         <div>
             <ul className={SNavbar.container_ul__listStyleNone}>
                 <li className={SNavbar.container_li__marginBottom}>
-                    <NavLink activeClassName={SNavbar.container_li__decorActive} className={SNavbar.container_li__decor} to='/profile'>My Profile</NavLink>
+                    <NavLink activeClassName={SNavbar.container_li__decorActive} className={SNavbar.container_li__decor} to='/profile'>Мой профиль</NavLink>
                 </li>
                 <li className={SNavbar.container_li__marginBottom}>
-                    <NavLink activeClassName={SNavbar.container_li__decorActive} className={SNavbar.container_li__decor} to='/common'>Common chat</NavLink>
+                    <NavLink activeClassName={SNavbar.container_li__decorActive} className={SNavbar.container_li__decor} to='/common'>Просто чат</NavLink>
                 </li>
                 <li className={SNavbar.container_li__marginBottom}>
-                    <NavLink activeClassName={SNavbar.container_li__decorActive} className={SNavbar.container_li__decor} to='/work'>Work chat</NavLink>
+                    <NavLink activeClassName={SNavbar.container_li__decorActive} className={SNavbar.container_li__decor} to='/work'>Рабочий чат</NavLink>
                 </li>
             </ul>
         </div>
